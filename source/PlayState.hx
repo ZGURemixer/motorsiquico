217ef2b4a3a5cb1b88838128560913648d697396
ZGURemixer <zonagamerultra@outlook.com>
2022-12-30 22:13:59 -0400

    re-added Week 1 stage

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -456,8 +456,8 @@ class PlayState extends MusicBeatState
 				// 	curStage = 'schoolEvil';
 				// case 'ugh' | 'guns' | 'stress':
 				// 	curStage = 'tank';
-				// default:
-				// 	curStage = 'stage';
+				default:
+					curStage = 'stage';
 			}
 		}
 		SONG.stage = curStage;
@@ -511,31 +511,31 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
-			// case 'stage': //Week 1
-			// 	var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
-			// 	add(bg);
+			case 'stage': //Week 1
+				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
+				add(bg);
 
-			// 	var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
-			// 	stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-			// 	stageFront.updateHitbox();
-			// 	add(stageFront);
-			// 	if(!ClientPrefs.lowQuality) {
-			// 		var stageLight:BGSprite = new BGSprite('stage_light', -125, -100, 0.9, 0.9);
-			// 		stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-			// 		stageLight.updateHitbox();
-			// 		add(stageLight);
-			// 		var stageLight:BGSprite = new BGSprite('stage_light', 1225, -100, 0.9, 0.9);
-			// 		stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
-			// 		stageLight.updateHitbox();
-			// 		stageLight.flipX = true;
-			// 		add(stageLight);
+				var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
+				stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+				stageFront.updateHitbox();
+				add(stageFront);
+				if(!ClientPrefs.lowQuality) {
+					var stageLight:BGSprite = new BGSprite('stage_light', -125, -100, 0.9, 0.9);
+					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
+					stageLight.updateHitbox();
+					add(stageLight);
+					var stageLight:BGSprite = new BGSprite('stage_light', 1225, -100, 0.9, 0.9);
+					stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
+					stageLight.updateHitbox();
+					stageLight.flipX = true;
+					add(stageLight);
 
-			// 		var stageCurtains:BGSprite = new BGSprite('stagecurtains', -500, -300, 1.3, 1.3);
-			// 		stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
-			// 		stageCurtains.updateHitbox();
-			// 		add(stageCurtains);
-			// 	}
-			// 	dadbattleSmokes = new FlxSpriteGroup(); //troll'd
+					var stageCurtains:BGSprite = new BGSprite('stagecurtains', -500, -300, 1.3, 1.3);
+					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
+					stageCurtains.updateHitbox();
+					add(stageCurtains);
+				}
+				// dadbattleSmokes = new FlxSpriteGroup(); //troll'd
 
 			// case 'spooky': //Week 2
 			// 	if(!ClientPrefs.lowQuality) {
